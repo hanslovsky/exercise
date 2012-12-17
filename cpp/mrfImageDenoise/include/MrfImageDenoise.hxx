4abaf661c7028720d6b695d659f008bd77f157f0
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-12-17 01:45:31 +0100

    added distance with metric as functor

--- cpp/mrfImageDenoise/include/MrfImageDenoise.hxx
+++ cpp/mrfImageDenoise/include/MrfImageDenoise.hxx
@@ -30,4 +30,25 @@ void randomVectorFromNormal(int N, double* numbers,
 		      double mean = 0.0, double variance = 1.0);
 
 
+
+// use functors to allow for various metrics
+
+template <typename T, typename BinaryFn>
+double distance(T p1, T p2, BinaryFn metric) { // = lTwoSquared<double>()) {
+  return metric(p1, p2);
+}
+
+template <typename T>
+class lTwoSquared {
+public:
+  lTwoSquared() {}
+  double operator() (T, T);
+};
+
+template <typename T>
+double lTwoSquared<T>::operator() (T p1, T p2) {
+  return static_cast<double> ((p1-p2)*(p1-p2));
+}
+
+
 #endif /* __MRF_IMAGE_DENOISE_HXX__ */
--- cpp/mrfImageDenoise/main.cxx
+++ cpp/mrfImageDenoise/main.cxx
@@ -7,5 +7,7 @@ using namespace std;
 int main() {
   NoisyImage im("map.png");
   im.gaussianNoiseImage(100, 0);
+  lTwoSquared<int> l2;
+  cout << distance<double, lTwoSquared<int> > (5.5, 3, l2) << "\n";
   return 0;
 }
