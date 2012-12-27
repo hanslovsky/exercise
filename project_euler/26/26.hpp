4dc54122d1ee24963f0a12cb53b48b62e95f70d4
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-12-27 13:58:18 +0100

    35 faster by a factor of 4

--- project_euler/26/26.hpp
+++ project_euler/26/26.hpp
@@ -31,6 +31,29 @@ int getNoDigits(int n) {
     return 10;
 }
 
+int getNoDigitsUInt(uint n) {
+  if (n < 10)
+    return 1;
+  if (n < 100)
+    return 2;
+  if (n < 1000)
+    return 3;
+  if (n < 10000)
+    return 4;
+  if (n < 100000)
+    return 5;
+  if (n < 1000000)
+    return 6;
+  if (n < 10000000)
+    return 7;
+  if (n < 100000000)
+    return 8;
+  if (n < 1000000000)
+    return 9;
+  else
+    return 10;
+}
+
 void division(int dividend, int divisor,
 	      double& result, int& periodLength) {
   int sign = 1;
