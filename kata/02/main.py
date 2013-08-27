1ed357efd6cce6b64cb27fc90d65b2a81f67fe32
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-08-27 09:04:56 +0200

    kata2 chop1 done

--- kata/02/main.py
+++ kata/02/main.py
@@ -3,19 +3,30 @@
 # http://codekata.pragprog.com/2007/01/kata_two_karate.html
 # write five different chop functions
 
+class NotInArrayException(Exception):
+    pass
 
 def chop1(comparison, array):
+    try:
+        return chop1_impl(comparison, array)
+    except NotInArrayException:
+        return False
+
+def chop1_impl(comparison, array):
     arr_size = len(array)
     if arr_size == 0:
-        return False
+        raise NotInArrayException("Not in Array!")
     elif arr_size == 1:
-        return comparison == array[0]
+        if comparison == array[0]:
+            return 0
+        else:
+            raise NotInArrayException("Not in Array!")
     elif array[arr_size/2] > comparison:
-        return chop1(comparison, array[:arr_size/2])
-    elif array[arr_size/2] < comparison:
-        return chop1(comparison, array[arr_size/2:])
-    else:
-        return True
+        return chop1_impl(comparison, array[:arr_size/2])
+    elif array[arr_size/2] <= comparison:
+        return chop1_impl(comparison, array[arr_size/2:]) + arr_size/2
+        
+
 
 
 def test_chop(chop_function):
@@ -28,20 +39,21 @@ def test_chop(chop_function):
 
     assert chop_function(-1, arr2) == False
     assert chop_function(0, arr2) == False
-    assert chop_function(1, arr2) == True
+    assert chop_function(1, arr2) == 0
     assert chop_function(4, arr2) == False
-    assert chop_function(5, arr2) == True
+    assert chop_function(5, arr2) == 2
 
     assert chop_function(-1, arr3) == False
     assert chop_function(0, arr3) == False
-    assert chop_function(1, arr3) == True
+    assert chop_function(1, arr3) == 0
     assert chop_function(4, arr3) == False
-    assert chop_function(5, arr3) == True
-    assert chop_function(7, arr3) == True
+    assert chop_function(5, arr3) == 2
+    assert chop_function(7, arr3) == 3
 
     print "Test success!"
 
 
 if __name__ == "__main__":
     test_chop(chop1)
+    print
 
